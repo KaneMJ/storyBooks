d35a7e34c87c1588f22271f726c3ba6a5ac53945
KaneMJ <matthewjkane@live.com>
2018-04-26 13:44:10 -0700

    for testing purposes

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://dev:devpass@ds259089.mlab.com:59089/storybooks',
+    googleClientID: '426726986786-0ilmin6q9c4d56uqmom04ddvpptigrtb.apps.googleusercontent.com',
+    googleClientSecret: '_vkiSCN4FnEiTvfUchMHk0ZL'
+}
